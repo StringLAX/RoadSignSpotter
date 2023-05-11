4a265bd0aeb9e2644be4c084433f0314dc09e5af
Vierza Vandifa <vierzavan2@gmail.com>
2023-05-11 18:58:38 +0700

    Pre Start

--- app/src/main/java/com/c23ps203/roadsignspotter/ui/MainActivity.kt
+++ app/src/main/java/com/c23ps203/roadsignspotter/ui/MainActivity.kt
@@ -3,10 +3,15 @@ package com.c23ps203.roadsignspotter.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.c23ps203.roadsignspotter.R
+import com.c23ps203.roadsignspotter.databinding.ActivityLoginBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
