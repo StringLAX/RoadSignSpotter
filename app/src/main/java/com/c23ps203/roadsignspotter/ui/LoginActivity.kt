5bb6e9f6bbd99dd59657374ea6493e46a255a5ad
Vierza Vandifa <vierzavan2@gmail.com>
2023-05-11 18:52:51 +0700

    Pre Start

--- app/src/main/java/com/c23ps203/roadsignspotter/ui/LoginActivity.kt
+++ app/src/main/java/com/c23ps203/roadsignspotter/ui/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.c23ps203.roadsignspotter.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.c23ps203.roadsignspotter.R
 import com.c23ps203.roadsignspotter.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -15,6 +16,27 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.title = "Road-Sign Spotter" + " | " + "Login"
+
+        binding.btnLogin.setOnClickListener {
+
+                val email = binding.edLoginEmail.text.toString()
+                val password = binding.edLoginPassword.text.toString()
+
+                when {
+                    email.isEmpty() || password.isEmpty() -> {
+                        binding.edLoginEmail.error = resources.getString(R.string.email_error)
+                        binding.edLoginPassword.error = resources.getString(R.string.password_error)
+                    }
+                    password.length < 8 -> {
+                        binding.edLoginPassword.error = resources.getString(R.string.password_error)
+                    }
+                    else -> {
+                        // testing intent
+                        intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+        }
 
         binding.btnToRegister.setOnClickListener {
             intent = Intent(this@LoginActivity, RegisterActivity::class.java)
