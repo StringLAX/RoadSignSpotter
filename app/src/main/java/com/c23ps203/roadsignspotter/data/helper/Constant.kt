37b49b881e978b9cc487deb44c00f4a8d3e3f6f5
Vierza Vandifa <vierzavan2@gmail.com>
2023-05-11 18:56:56 +0700

    Pre Start

---
+++ app/src/main/java/com/c23ps203/roadsignspotter/data/helper/Constant.kt
@@ -0,0 +1,13 @@
+package com.c23ps203.roadsignspotter.data.helper
+
+import com.c23ps203.roadsignspotter.R
+
+class Constant {
+
+    companion object {
+        const val prefIsLogin = R.string.pref_is_login.toString()
+        const val prefToken = R.string.pref_token.toString()
+        const val prefName = R.string.pref_name.toString()
+        const val prefUserId = R.string.pref_user_id.toString()
+    }
+}
---
+++ app/src/main/java/com/c23ps203/roadsignspotter/data/helper/PreferenceHelper.kt
@@ -0,0 +1,28 @@
+package com.c23ps203.roadsignspotter.data.helper
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferenceHelper(context: Context) {
+    private val sharedPref: SharedPreferences
+    private val editor: SharedPreferences.Editor
+
+    init {
+        sharedPref = context.getSharedPreferences("RoadSignSpotter", Context.MODE_PRIVATE)
+        editor = sharedPref.edit()
+    }
+
+    fun put(key: String, value: String) {
+        editor.putString(key, value)
+        editor.apply()
+    }
+
+    fun getString(key: String): String? {
+        return sharedPref.getString(key, null)
+    }
+
+    fun clear() {
+        editor.clear()
+        editor.apply()
+    }
+}
